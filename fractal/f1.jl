8fbce38625261252fa0b924f0b795e68cf817666
Pablo Schwarzenberg <pabloschwarzenberg@gmail.com>
2022-09-05 17:35:55 -0400

    ga

--- fractal/f1.jl
+++
@@ -1,9 +0,0 @@
-using Pkg
-Pkg.add("Fatou")
-Pkg.add("PyPlot")
-using Fatou
-using PyPlot
-
-c = -0.06 + 0.67im
-nf = juliafill(:(z^2+$c),∂=[-1.5,1.5,-1,1],N=80,n=1501,cmap="gnuplot",iter=true)
-plot(fatou(nf), bare=true)
