7ed824495994a7c5a80f6c85b2f82c47d75c55b0
Pablo Schwarzenberg <pabloschwarzenberg@gmail.com>
2023-09-26 20:08:06 -0300

    optic+

--- planning/optic/setup.sh
+++ planning/optic/setup.sh
@@ -5,5 +5,6 @@ apt-get -y install cmake
 apt-get -y install zlib1g-dev
 apt-get -y install libgsl-dev
 apt-get -y install coinor-libcbc-dev coinor-libclp-dev coinor-libcoinutils-dev coinor-libosi-dev coinor-libcgl-dev
+mkdir release
 ./run-cmake-release
 ./build-release
